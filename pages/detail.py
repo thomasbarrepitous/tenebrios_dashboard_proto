022808a963151d85314b4094fbb6af380216fcd9
Thomas Barré-Pitous <barrepitousthomas@gmail.com>
2023-09-05 05:03:33 +0900

    Layout detail improvement

--- pages/detail.py
+++ pages/detail.py
@@ -10,7 +10,7 @@ from datetime import datetime
 dash.register_page(__name__, path_template="/tracability/<recolte_nb>")
 
 
-details = []
+actions = []
 
 API_URL = f"http://127.0.0.1:8000/api/actions"
 
@@ -21,16 +21,16 @@ with open("auth.json") as auth_file:
 
 
 def layout(recolte_nb=None):
-    return display_details(recolte_nb)
+    return display_layout(recolte_nb)
 
 
-def map_values_polymorphictype(key: int) -> str | None:
+def ctype_to_name(key: int) -> str | None:
     map = {
         11: "Mise en culture",
         12: "Nourrisage Humide",
         13: "Nourrisage Son",
         14: "Recolte",
-        15: "Tamisage",
+        # 15: "Tamisage",
     }
     return map.get(key)
 
@@ -42,12 +42,12 @@ def get_details_from_recolte_nb(recolte_nb):
     )
 
 
-def display_detail(action):
+def action_item_component(action):
     action_date = datetime.strptime(action["date"], "%Y-%m-%dT%H:%M:%S.%fZ")
     return dmc.AccordionItem(
         [
             dmc.AccordionControl(
-                f'{map_values_polymorphictype(action["polymorphic_ctype"])} du {action_date.strftime("%B %d, %Y")}'
+                f'{ctype_to_name(action["polymorphic_ctype"])} du {action_date.strftime("%B %d, %Y")}'
             ),
             dmc.AccordionPanel(action["date"]),
         ],
@@ -55,27 +55,64 @@ def display_detail(action):
     )
 
 
-def display_details(recolte_nb):
+def display_top_title(actions):
+    return [dbc.Row(html.H2(actions[0]["recolte_nb"], className="text-center"))]
+
+
+def display_indicators_layout():
+    return [
+        dbc.Row(dmc.Divider(label=html.H3("Indicateurs"), labelPosition="center")),
+        dbc.Row(
+            html.H4("Indicateurs globaux"),
+            className="text-center text-decoration-underline",
+        ),
+        dbc.Row(
+            [
+                dbc.Col([html.H4(f"Date de mise en culture"), html.P("13 aout 3932")]),
+                dbc.Col([html.H4(f"Date de recolte"), html.P("14 aout 1093")]),
+                dbc.Col([html.H4(f"Nombre de bacs"), html.P("111")]),
+                dbc.Col([html.H4(f"Quantite recoltee"), html.P("11")]),
+            ]
+        ),
+        dbc.Row(
+            html.H4("Indicateurs moyens"),
+            className="text-center text-decoration-underline",
+        ),
+        dbc.Row(
+            [
+                dbc.Col([html.H4(f"IWM à la récolte"), html.P("13 aout 3932")]),
+                dbc.Col([html.H4(f"Son total donné"), html.P("13 aout 3932")]),
+                dbc.Col(
+                    [
+                        html.H4(f"Nourriture humide totale donnée"),
+                        html.P("13 aout 3932"),
+                    ]
+                ),
+                dbc.Col([html.H4(f"Feed Ratio Conversion "), html.P("13 aout 3932")]),
+                dbc.Col(
+                    [html.H4(f"Croissance journalière moyenne"), html.P("13 aout 3932")]
+                ),
+                dbc.Col([html.H4(f"Assimilation moyenne "), html.P("13 aout 3932")]),
+            ]
+        ),
+    ]
+
+
+def display_historical_actions_layout(actions):
+    return [
+        dbc.Row(
+            dmc.Divider(label=html.H3("Toutes les actions"), labelPosition="center")
+        ),
+        dbc.Row(
+            [dmc.Accordion(action_item_component(action)) for action in actions],
+        ),
+    ]
+
+
+def display_layout(recolte_nb):
     actions = get_details_from_recolte_nb(recolte_nb)
     return dbc.Container(
-        [
-            dbc.Row(
-                dbc.Col(
-                    html.H2(actions[0]["recolte_nb"]), width={"size": 3, "offset": 5}
-                )
-            ),
-            dbc.Row(dmc.Divider(label=html.H3("Indicateurs"), labelPosition="center")),
-            dbc.Row(
-                [
-                    dbc.Col(html.H4("okdawkdwa")),
-                    dbc.Col(html.H4("daiwj")),
-                ]
-            ),
-            dbc.Row(
-                dmc.Divider(label=html.H3("Toutes les actions"), labelPosition="center")
-            ),
-            dbc.Row(
-                [dmc.Accordion(display_detail(action)) for action in actions],
-            ),
-        ]
+        display_top_title(actions)
+        + display_indicators_layout()
+        + display_historical_actions_layout(actions)
     )
